21163b73a10b1b098866b795ffcf35bd2fad66a0
Adrian Naranjo <adrian-naranjo@users.noreply.github.com>
2016-12-02 10:30:18 -0500

    Update PushToTeamYearStats.py

--- PushToTeamYearStats.py
+++ PushToTeamYearStats.py
@@ -4,13 +4,21 @@ import sqlite3
 import re
 import time
 
-print (time.asctime(time.localtime(time.time())))
 
+# currentSchoolYear should be year which the current or last college football season began.
+# So, if it is the 2016-2017 season, you would enter 2016.
+currentSchoolYear = 2016
+
+# Used for testing. If "Yes" only a subsection of schools will be scraped and populated in the database
 isThisATest = 'no'
 
-thisYear = 2010
 
-conn = sqlite3.connect('BettingData.db')
+#########################################################################
+# Below creates tables ready to receive data for the Coach, Conference, and Team Splits tables
+print (time.asctime(time.localtime(time.time())))
+thisYear = (currentSchoolYear - 9)
+
+conn = sqlite3.connect('CFBData.db')
 cur = conn.cursor()
 
 cur.executescript('''
@@ -22,32 +30,39 @@ CREATE TABLE TeamStatistics (
     ,Year TEXT
     ,Side TEXT
     ,GamesPlayed INTEGER
-    ,offensePassingCompletions REAL
-    ,offensePassingAtmpts REAL
-    ,offensePassingCompletePct REAL
-    ,offensePassingTotYds REAL
-    ,offensePassingTDs REAL
-    ,offenseRushingAtmpts REAL
-    ,offenseRushingTotYds REAL
-    ,offenseRushingAvgYds REAL
-    ,offenseRushingTDs REAL
-    ,totalOffensePlays REAL
-    ,totalOffenseTotYds REAL
-    ,totalOffenseAvgYds REAL
-    ,firstDownsByPassing REAL
-    ,firstDownsByRushing REAL
-    ,firstDownsByPenalty REAL
-    ,firstDownsTot REAL
-    ,penaltiesTot REAL
-    ,penaltiesYds REAL
-    ,turnoversByFumble REAL
-    ,turnoversByInt REAL
-    ,turnoversTot REAL
+    ,PassingCompletions REAL
+    ,PassingAtmpts REAL
+    ,PassingCompletePct REAL
+    ,PassingTotYds REAL
+    ,PassingTDs REAL
+    ,RushingAtmpts REAL
+    ,RushingTotYds REAL
+    ,RushingAvgYds REAL
+    ,RushingTDs REAL
+    ,TotalOffensePlays REAL
+    ,TotalOffenseTotYds REAL
+    ,TotalOffenseAvgYds REAL
+    ,FirstDownsByPassing REAL
+    ,FirstDownsByRushing REAL
+    ,FirstDownsByPenalty REAL
+    ,FirstDownsTot REAL
+    ,PenaltiesTot REAL
+    ,PenaltiesYds REAL
+    ,TurnoversByFumble REAL
+    ,TurnoversByInt REAL
+    ,TurnoversTot REAL
 
 );
 
 ''')
 
+print ('An empty TeamStatistics table has been created')
+
+
+#########################################################################
+
+#This section creates a list of schools playing in the current school year
+#The list is used to generate weblinks which are visited and scraped below
 collegesURL = ('http://www.sports-reference.com/cfb/schools/')
 
 if isThisATest != 'yes':
@@ -60,7 +75,7 @@ if isThisATest != 'yes':
     colleges = list()
     
     for tag in collegesTags:
-#        print ('Tag:',tag)
+
         if re.search('.+/cfb/schools/.+', str(tag)):
             collegesString = str(tag.contents)[2:-2]
             collegesString = str(collegesString).lower().strip()
@@ -70,8 +85,10 @@ if isThisATest != 'yes':
         else: continue
 else: colleges = ['alabama','florida-international']
 
-#print (colleges)
 
+################################################################################
+
+#Defines a function to populate data in the Team Statistics table. The function is called in the next section. 
 def PushToDB(collegeName,year,side,gamesPlayed,offensePassingCompletions\
         ,offensePassingAtmpts,offensePassingCompletePct,offensePassingTotYds\
         ,offensePassingTDs,offenseRushingAtmpts,offenseRushingTotYds,offenseRushingAvgYds\
@@ -79,16 +96,16 @@ def PushToDB(collegeName,year,side,gamesPlayed,offensePassingCompletions\
         ,firstDownsByPassing,firstDownsByRushing,firstDownsByPenalty,firstDownsTot\
         ,penaltiesTot,penaltiesYds,turnoversByFumble,turnoversByInt,turnoversTot):
         
-        conn = sqlite3.connect('BettingData.db')
+        conn = sqlite3.connect('CFBData.db')
         cur = conn.cursor()
     
         cur.execute('''
-            INSERT INTO TeamStatistics (SchoolName,year,side,gamesPlayed,offensePassingCompletions
-            ,offensePassingAtmpts,offensePassingCompletePct,offensePassingTotYds
-            ,offensePassingTDs,offenseRushingAtmpts,offenseRushingTotYds,offenseRushingAvgYds
-            ,offenseRushingTDs,totalOffensePlays,totalOffenseTotYds,totalOffenseAvgYds
-            ,firstDownsByPassing,firstDownsByRushing,firstDownsByPenalty,firstDownsTot
-            ,penaltiesTot,penaltiesYds,turnoversByFumble,turnoversByInt,turnoversTot)
+            INSERT INTO TeamStatistics (SchoolName,Year,Side,GamesPlayed,PassingCompletions
+            ,PassingAtmpts,PassingCompletePct,PassingTotYds
+            ,PassingTDs,RushingAtmpts,RushingTotYds,RushingAvgYds
+            ,RushingTDs,TotalOffensePlays,TotalOffenseTotYds,TotalOffenseAvgYds
+            ,FirstDownsByPassing,FirstDownsByRushing,FirstDownsByPenalty,FirstDownsTot
+            ,PenaltiesTot,PenaltiesYds,TurnoversByFumble,TurnoversByInt,TurnoversTot)
             VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
             (collegeName,year,side,gamesPlayed,offensePassingCompletions\
             ,offensePassingAtmpts,offensePassingCompletePct,offensePassingTotYds\
@@ -99,18 +116,18 @@ def PushToDB(collegeName,year,side,gamesPlayed,offensePassingCompletions\
             )
         
         conn.commit()
+        
+        
+#########################################################################        
 
-collegesLinks = list()
-
+#This section iterates through weblinks using the school list above which contain game data
+#and populates the TeamSplits data in the database     
+print ('Populating the TeamStatistics table. This may take several hours.')
 for college in colleges:
-    
-    print (college)
     
     year = thisYear
     
     while year <= 2015:
-        
-        print (year)
         
         collegeName = college    
         
@@ -118,23 +135,24 @@ for college in colleges:
         +str(college)+'/'+str(year)+'.html'
     
         try: collegeStatsHTML = urllib.request.urlopen(collegeURL).read()
-        except: continue
+        except: 
+            year = year + 1
+            continue
         
-        collegeStatsSoup = BeautifulSoup(collegeStatsHTML, 'html.parser')
-    #    collegeStatsTags = collegeStatsSoup('table', id='team')  
+        collegeStatsSoup = BeautifulSoup(collegeStatsHTML, 'html.parser')    
         collegeStatsTags = collegeStatsSoup('td')  
         
         side = ''
         gamesPlayed = ''
-        offensePassingCompletions = ''
-        offensePassingAtmpts = ''
-        offensePassingCompletePct = ''
-        offensePassingTotYds = ''
-        offensePassingTDs = ''
-        offenseRushingAtmpts = ''
-        offenseRushingTotYds = ''
-        offenseRushingAvgYds = ''
-        offenseRushingTDs = ''
+        PassingCompletions = ''
+        PassingAtmpts = ''
+        PassingCompletePct = ''
+        PassingTotYds = ''
+        PassingTDs = ''
+        RushingAtmpts = ''
+        RushingTotYds = ''
+        RushingAvgYds = ''
+        RushingTDs = ''
         totalOffensePlays = ''
         totalOffenseTotYds = ''
         totalOffenseAvgYds = ''
@@ -153,74 +171,72 @@ for college in colleges:
         
         for tag in collegeStatsTags:
             if x == 0:
-                side = (str(tag.contents)[2:-2])
-            elif x == 1:
                 gamesPlayed = (str(tag.contents)[2:-2])
+            elif x == 1:
+                PassingCompletions = (str(tag.contents)[2:-2])
             elif x == 2:
-                offensePassingCompletions = (str(tag.contents)[2:-2])
+                PassingAtmpts = (str(tag.contents)[2:-2])
             elif x == 3:
-                offensePassingAtmpts = (str(tag.contents)[2:-2])
+                PassingCompletePct = (str(tag.contents)[2:-2])
             elif x == 4:
-                offensePassingCompletePct = (str(tag.contents)[2:-2])
+                PassingTotYds = (str(tag.contents)[2:-2])
             elif x == 5:
-                offensePassingTotYds = (str(tag.contents)[2:-2])
+                PassingTDs = (str(tag.contents)[2:-2])
             elif x == 6:
-                offensePassingTDs = (str(tag.contents)[2:-2])
+                RushingAtmpts = (str(tag.contents)[2:-2])
             elif x == 7:
-                offenseRushingAtmpts = (str(tag.contents)[2:-2])
+                RushingTotYds = (str(tag.contents)[2:-2])
             elif x == 8:
-                offenseRushingTotYds = (str(tag.contents)[2:-2])
+                RushingAvgYds = (str(tag.contents)[2:-2])
             elif x == 9:
-                offenseRushingAvgYds = (str(tag.contents)[2:-2])
+                RushingTDs = (str(tag.contents)[2:-2])
             elif x == 10:
-                offenseRushingTDs = (str(tag.contents)[2:-2])
-            elif x == 11:
                 totalOffensePlays = (str(tag.contents)[2:-2])
-            elif x == 12:
+            elif x == 11:
                 totalOffenseTotYds = (str(tag.contents)[2:-2])
-            elif x == 13:
+            elif x == 12:
                 totalOffenseAvgYds = (str(tag.contents)[2:-2])
-            elif x == 14:
+            elif x == 13:
                 firstDownsByPassing = (str(tag.contents)[2:-2])
-            elif x == 15:
+            elif x == 14:
                 firstDownsByRushing = (str(tag.contents)[2:-2])
-            elif x == 16:
+            elif x == 15:
                 firstDownsByPenalty = (str(tag.contents)[2:-2])
-            elif x == 17:
+            elif x == 16:
                 firstDownsTot = (str(tag.contents)[2:-2])
-            elif x == 18:
+            elif x == 17:
                 penaltiesTot = (str(tag.contents)[2:-2])
-            elif x == 19:
+            elif x == 18:
                 penaltiesYds = (str(tag.contents)[2:-2])
-            elif x == 20:
+            elif x == 19:
                 turnoversByFumble = (str(tag.contents)[2:-2])
-            elif x == 21:
+            elif x == 20:
                 turnoversByInt = (str(tag.contents)[2:-2])
-            elif x == 22:
+            elif x == 21:
                 turnoversTot = (str(tag.contents)[2:-2])
                 
             x = x + 1
-    #        print ('X:',x)
-    #        print ('Y:',y)
             
-            if x == 23 and y == 1:
-    #            print ('1')            
+            if x == 22 and y == 1:
+
+                side = 'Defense'           
                 
-                PushToDB(collegeName,year,side,gamesPlayed,offensePassingCompletions\
-                    ,offensePassingAtmpts,offensePassingCompletePct,offensePassingTotYds\
-                    ,offensePassingTDs,offenseRushingAtmpts,offenseRushingTotYds,offenseRushingAvgYds\
-                    ,offenseRushingTDs,totalOffensePlays,totalOffenseTotYds,totalOffenseAvgYds\
+                PushToDB(collegeName,year,side,gamesPlayed,PassingCompletions\
+                    ,PassingAtmpts,PassingCompletePct,PassingTotYds\
+                    ,PassingTDs,RushingAtmpts,RushingTotYds,RushingAvgYds\
+                    ,RushingTDs,totalOffensePlays,totalOffenseTotYds,totalOffenseAvgYds\
                     ,firstDownsByPassing,firstDownsByRushing,firstDownsByPenalty,firstDownsTot\
                     ,penaltiesTot,penaltiesYds,turnoversByFumble,turnoversByInt,turnoversTot)
                 continue
             
-            if x == 23 and y == 0:
-    #            print ('2')            
+            if x == 22 and y == 0:
+
+                side = 'Offense'           
                 
-                PushToDB(collegeName,year,side,gamesPlayed,offensePassingCompletions\
-                    ,offensePassingAtmpts,offensePassingCompletePct,offensePassingTotYds\
-                    ,offensePassingTDs,offenseRushingAtmpts,offenseRushingTotYds,offenseRushingAvgYds\
-                    ,offenseRushingTDs,totalOffensePlays,totalOffenseTotYds,totalOffenseAvgYds\
+                PushToDB(collegeName,year,side,gamesPlayed,PassingCompletions\
+                    ,PassingAtmpts,PassingCompletePct,PassingTotYds\
+                    ,PassingTDs,RushingAtmpts,RushingTotYds,RushingAvgYds\
+                    ,RushingTDs,totalOffensePlays,totalOffenseTotYds,totalOffenseAvgYds\
                     ,firstDownsByPassing,firstDownsByRushing,firstDownsByPenalty,firstDownsTot\
                     ,penaltiesTot,penaltiesYds,turnoversByFumble,turnoversByInt,turnoversTot)
                 x = 0
@@ -228,5 +244,6 @@ for college in colleges:
          
         year = year + 1
 
-# print ("Committed to database")
+print ("TeamStatistics table has been populated")
+
 print (time.asctime(time.localtime(time.time())))
