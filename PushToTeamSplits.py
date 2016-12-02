f7c0acb97ec2be51a62b495be1b3ed36bda8e60c
Adrian Naranjo <adrian-naranjo@users.noreply.github.com>
2016-12-02 10:29:49 -0500

    Update PushToTeamSplits.py

--- PushToTeamSplits.py
+++ PushToTeamSplits.py
@@ -4,20 +4,27 @@ import sqlite3
 import re
 import time
 
+# currentSchoolYear should be year which the current or last college football season began.
+# So, if it is the 2016-2017 season, you would enter 2016.
+currentSchoolYear = 2016
+
+# Used for testing. If "Yes" only a subsection of schools will be scraped and populated in the database
+isThisATest = 'yes'
+
+
+#########################################################################
+# Below creates tables ready to receive data for the Coach, Conference, and Team Splits tables
 print (time.asctime(time.localtime(time.time())))
+thisYear = (currentSchoolYear - 9)
 
-isThisATest = 'no'
-
-thisYear = 2006
-
-conn = sqlite3.connect('BettingData.db')
+conn = sqlite3.connect('CFBData.db')
 cur = conn.cursor()
 
 cur.executescript('''
 
 DROP TABLE IF EXISTS TeamSplits;
---DROP TABLE IF EXISTS Conference;
---DROP TABLE IF EXISTS Coach;
+DROP TABLE IF EXISTS Conference;
+DROP TABLE IF EXISTS Coach;
 
 CREATE TABLE TeamSplits (
     SchoolName TEXT
@@ -50,7 +57,7 @@ CREATE TABLE TeamSplits (
     ,TotalTurnovers REAL
 );
 
-/*
+
 CREATE TABLE Conference (
     SchoolName TEXT
     ,Year INTEGER
@@ -62,14 +69,19 @@ CREATE TABLE Coach (
     ,Year INTEGER
     ,Coach TEXT
 );
-*/
+
 
 ''')
 
 conn.close()
 
+print ('Empty Coach, Conference, and TeamSplits tables have been created')
+
 ###############################################################################
 
+
+#This section creates a list of schools playing in the current school year
+#The list is used to generate weblinks which are visited and scraped below
 collegesURL = ('http://www.sports-reference.com/cfb/schools/')
 
 if isThisATest != 'yes':
@@ -89,102 +101,104 @@ if isThisATest != 'yes':
             collegesString = re.sub('\s','-',collegesString)
             collegesString = re.sub('[()]','',collegesString)
             collegesString = re.sub('&amp;','',collegesString)
-        if re.search('2015', str(tag)):
+        if re.search(str(currentSchoolYear), str(tag)):
             colleges.append(collegesString)
-    #        print (collegesString)
         else: continue
     
-#    print (colleges) 
 
-else: colleges = ['air-force']
+else: colleges = ['alabama']
 
-print ('Done with Schools')
+print ('Created a list of schools')
 
-        
+     
 ########################################################################################
+
+#Defines a function to populate data in the Conference table. The function is called below, after 
+# the function to populate the Coach table has also been defined.      
+def PushToConference(collegeName, year, conference):
+    
+        conn = sqlite3.connect('CFBData.db')
+        cur = conn.cursor()
+    
+        cur.execute('''
+            INSERT INTO Conference (SchoolName,Year,Conference)
+            VALUES (?, ?, ?)''',
+            (collegeName, year, conference)
+            )
         
-#def PushToConference(collegeName, year, conference):
-#    
-#        conn = sqlite3.connect('BettingData.db')
-#        cur = conn.cursor()
-#    
-#        cur.execute('''
-#            INSERT INTO Conference (SchoolName,Year,Conference)
-#            VALUES (?, ?, ?)''',
-#            (collegeName, year, conference)
-#            )
-#        
-#        
-#        conn.commit()
-#        
-#        conn.close()
+        
+        conn.commit()
+        
+        conn.close()
      
 #####################################################################################
-#        
-#def PushToCoach(collegeName, year, coach):
-#    
-#        conn = sqlite3.connect('BettingData.db')
-#        cur = conn.cursor()
-#
-#        cur.execute('''
-#            INSERT INTO Coach (SchoolName,Year,Coach)
-#            VALUES (?, ?, ?)''',
-#            (collegeName, year, coach)
-#            )
-#        
-#        
-#        conn.commit()
-#        
-#        conn.close()
-        
-#########################################################################################        
+
+#Defines a function to populate data in the Coach table. The function is called in the next section.        
+def PushToCoach(collegeName, year, coach):
     
-#for college in colleges:
-#
-#    year = thisYear 
-#    
-#    conference = ''
-#    coach = ''
-#    
-#    collegeName = str(college)
-#        
-#    while year <= 2015:
-#        collegeURLYear = ('http://www.sports-reference.com/cfb/schools/')\
-#        +college+'/'
-#
-#        try: 
-#            collegeStatsYearHTML = urllib.request.urlopen(collegeURLYear).read()
-#        except: continue
-#    
-#        collegeYearSoup = BeautifulSoup(collegeStatsYearHTML, 'html.parser')
-#    #    collegeStatsTags = collegeStatsSoup('table', id='team')  
-#        collegeYearTags = collegeYearSoup('td')
-#        
-#        
-#        for tag in collegeYearTags:
-##            print (tag)
-#            if re.search(str(year), str(tag.contents)):
-#                conferenceString = str(tag.contents)
-#                if re.search('>[A-Za-z].+',conferenceString):
-#                    conferenceString = re.findall('>[A-Za-z].+',conferenceString)
-#                    conference = str(conferenceString)[3:-7]
-#                    PushToConference(collegeName, year, conference)
-#                    continue
-##                    print (conference)
-#            if re.search('coaches',str(tag)) and re.search(str(year), str(tag)):
-#                coachString = re.findall('>[A-Z].+?<',str(tag.contents))
-##                print ('1',coachString)
-#                for coaches in coachString:
-#                    coach = str(coaches)[1:-1]
-##                    print ('2',coach)
-#                    PushToCoach(collegeName, year, coach)
-#                    
-#        year = year + 1
-#        
-#print ('Done with Conferences & Coaches')
+        conn = sqlite3.connect('CFBData.db')
+        cur = conn.cursor()
+
+        cur.execute('''
+            INSERT INTO Coach (SchoolName,Year,Coach)
+            VALUES (?, ?, ?)''',
+            (collegeName, year, coach)
+            )
+        
+        
+        conn.commit()
+        
+        conn.close()
+        
+#########################################################################################  
+      
+#This section iterates through weblinks using the school list above which contain conference and coach data 
+#and populates the Conference and Coach data in the database 
+print ('Populating Coach and Conference tables. May take several minutes.')
+
+for college in colleges:
+
+    year = thisYear 
+    
+    conference = ''
+    coach = ''
+    
+    collegeName = str(college)
+        
+    while year <= currentSchoolYear:
+        collegeURLYear = ('http://www.sports-reference.com/cfb/schools/')\
+        +college+'/'
+
+        try: 
+            collegeStatsYearHTML = urllib.request.urlopen(collegeURLYear).read()
+        except: continue
+    
+        collegeYearSoup = BeautifulSoup(collegeStatsYearHTML, 'html.parser') 
+        collegeYearTags = collegeYearSoup('td')
+        
+        
+        for tag in collegeYearTags:
+            if re.search(str(year), str(tag.contents)):
+                conferenceString = str(tag.contents)
+                if re.search('>[A-Za-z].+',conferenceString):
+                    conferenceString = re.findall('>[A-Za-z].+',conferenceString)
+                    conference = str(conferenceString)[3:-7]
+                    PushToConference(collegeName, year, conference)
+                    continue
+            if re.search('coaches',str(tag)) and re.search(str(year), str(tag)):
+                coachString = re.findall('>[A-Z].+?<',str(tag.contents))
+                for coaches in coachString:
+                    coach = str(coaches)[1:-1]
+                    PushToCoach(collegeName, year, coach)
+                    
+        year = year + 1
+        
+print ('Conference and Coach tables have been populated')
+print (time.asctime(time.localtime(time.time())))
 
 ###############################################################################
 
+#Defines a function to populate data in the TeamSplits table. The function is called in the next section.  
 def PushToDB(collegeName,year,side,value,gamesPlayed,wins,losses,offensePassingCompletions\
         ,offensePassingAtmpts,offensePassingCompletePct,offensePassingTotYds\
         ,offensePassingTDs,offenseRushingAtmpts,offenseRushingTotYds,offenseRushingAvgYds\
@@ -192,7 +206,7 @@ def PushToDB(collegeName,year,side,value,gamesPlayed,wins,losses,offensePassingC
         ,firstDownsByPassing,firstDownsByRushing,firstDownsByPenalty,firstDownsTot\
         ,penaltiesTot,penaltiesYds,turnoversByFumble,turnoversByInt,turnoversTot):
         
-        conn = sqlite3.connect('BettingData.db')
+        conn = sqlite3.connect('CFBData.db')
         cur = conn.cursor()
     
         cur.execute('''
@@ -211,24 +225,26 @@ def PushToDB(collegeName,year,side,value,gamesPlayed,wins,losses,offensePassingC
             ,penaltiesTot,penaltiesYds,turnoversByFumble,turnoversByInt,turnoversTot)
             )
         
-#        print ('written')
-        
         conn.commit()
         
         conn.close()
         
 ################################################################################
-              
+
+#This section iterates through weblinks using the school list above which contain game data
+#and populates the TeamSplits data in the database     
+print ('Populating the TeamSplits table. This may take several hours.')
+         
 for college in colleges:
     
-    print (college)
+    #print (college)
     collegeName = str(college)
         
     year = thisYear
     
-    while year <= 2015:
+    while year <= currentSchoolYear:
         
-        print (year)
+        #print (year)
 
         collegeURL = ('http://www.sports-reference.com/cfb/schools/')\
         +college+'/'+str(year)+'/splits/'
@@ -236,8 +252,7 @@ for college in colleges:
         try: collegeStatsHTML = urllib.request.urlopen(collegeURL).read()
         except: continue
         
-        collegeStatsSoup = BeautifulSoup(collegeStatsHTML, 'html.parser')
-    #    collegeStatsTags = collegeStatsSoup('table', id='team')  
+        collegeStatsSoup = BeautifulSoup(collegeStatsHTML, 'html.parser')  
         collegeStatsTags = collegeStatsSoup('td')  
 
         placeFlag = ''        
@@ -305,82 +320,56 @@ for college in colleges:
             if run == 1:
                 if x == 0:
                     value = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 1:
                     gamesPlayed = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 2:
                     wins = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 3:
                     losses = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 4:
                     offensePassingCompletions = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 5:
                     offensePassingAtmpts = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 6:
                     offensePassingCompletePct = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 7:
                     offensePassingTotYds = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 8:
                     offensePassingTDs = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 9:
                     offenseRushingAtmpts = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 10:
                     offenseRushingTotYds = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 11:
                     offenseRushingAvgYds = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 12:
                     offenseRushingTDs = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 13:
                     totalOffensePlays = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 14:
                     totalOffenseTotYds = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 15:
                     totalOffenseAvgYds = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 16:
                     firstDownsByPassing = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 17:
                     firstDownsByRushing = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 18:
                     firstDownsByPenalty = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 19:
                     firstDownsTot = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 20:
                     penaltiesTot = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 21:
                     penaltiesYds = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 22:
                     turnoversByFumble = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 23:
                     turnoversByInt = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                 elif x == 24:
                     turnoversTot = (str(tag.contents)[2:-2])
-#                    print (str(tag.contents)[2:-2])
                     
                 x = x + 1 
-#                print ('X:',x)
                     
                 if x == 25:
                     PushToDB(collegeName,year,side,value,gamesPlayed,wins,losses,offensePassingCompletions\
@@ -393,12 +382,9 @@ for college in colleges:
                     x = 0
                     run = 0
                     
-#                    if ((win == 1) and (loss == 1) and (home == 1) and (road == 1) and (neutral == ''))\
-#                    or ((win == 1) and (loss == 1) and (home == 1) and (road == 1) and (neutral == 1)):
-#                        side = 'Defense'
     
         year = year + 1
 
-# print ("Committed to database")
+print ("Team Splits table has been populated")
 
 print (time.asctime(time.localtime(time.time())))
